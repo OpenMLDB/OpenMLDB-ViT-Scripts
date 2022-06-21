600a8df755c3ec62a6a949fcaae23d2d216c48e7
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-06-21 15:09:48 +0000

    better makefile

--- precision-benchmarks/torchvision-vit16-large/01_pytorch-fp32.py
+++ precision-benchmarks/torchvision-vit16-large/01_pytorch-fp32.py
@@ -8,4 +8,9 @@ from torchvision import transforms
 from torchvision.models import vit_l_16
 from torchvision.models import ViT_L_16_Weights
 
-from local_u
+from local_utilities import get_dataloaders_cifar10
+
+
+def train(num_epochs, model, optimizer, train_loader, val_loader, device, scheduler):
+
+ 
