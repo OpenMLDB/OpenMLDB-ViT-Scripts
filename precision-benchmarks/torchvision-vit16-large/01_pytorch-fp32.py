5bc9f819f2283eb8af15e35cbb91f9be255abb56
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-06-21 13:08:54 +0000

    [pt] rules improvements

--- precision-benchmarks/torchvision-vit16-large/01_pytorch-fp32.py
+++ precision-benchmarks/torchvision-vit16-large/01_pytorch-fp32.py
@@ -4,4 +4,8 @@ import torch
 import torch.nn.functional as F
 from torch.optim.lr_scheduler import ExponentialLR
 import torchmetrics
-from torchvi
+from torchvision import transforms
+from torchvision.models import vit_l_16
+from torchvision.models import ViT_L_16_Weights
+
+from local_u
