7c69ef9c464a6cfa6999e07b67eb1fa03be46ba5
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-06-29 18:11:27 +0000

    MIPS: Fix enabling of DEBUG_STACKOVERFLOW

--- precision-benchmarks/torchvision-vit16-large/01_pytorch-fp32.py
+++ precision-benchmarks/torchvision-vit16-large/01_pytorch-fp32.py
@@ -34,4 +34,6 @@ def train(num_epochs, model, optimizer, train_loader, val_loader, device, schedu
             if not batch_idx % 300:
                 print(f"Epoch: {epoch+1:04d}/{num_epochs:04d} | Batch {batch_idx:04d}/{len(train_loader):04d} | Loss: {loss:.4f}")
 
-    
+            model.eval()
+            with torch.no_grad():
+                predicted_labels = torch.argmax(logits, 
