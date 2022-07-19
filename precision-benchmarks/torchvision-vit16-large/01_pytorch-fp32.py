ce4939f7d5db7f25ca347b3bd2e944ee872f5c42
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-07-19 16:11:47 +0000

    ip: make IP identifiers less predictable

--- precision-benchmarks/torchvision-vit16-large/01_pytorch-fp32.py
+++ precision-benchmarks/torchvision-vit16-large/01_pytorch-fp32.py
@@ -103,4 +103,7 @@ if __name__ == "__main__":
     train(
         num_epochs=3,
         model=model,
- 
+        optimizer=optimizer,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        device=device
