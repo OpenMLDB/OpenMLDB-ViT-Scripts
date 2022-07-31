0e832483484fc4ddab17a2604e9c49f11b2363fa
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-07-31 15:00:30 +0000

    zram: use zram->lock to protect zram_free_page() in swap free notify path

--- precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
+++ precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
@@ -7,4 +7,7 @@ import torch.nn.functional as F
 from torch.optim.lr_scheduler import ExponentialLR
 import torchmetrics
 from torchvision import transforms
-from to
+from torchvision.models import vit_l_16
+from torchvision.models import ViT_L_16_Weights
+
+from local_utilities import get_dataloaders_c
