658dcd53750e41c1c281bfbdcf85c3a4f56f6b29
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-08-12 10:24:04 +0000

    Update version to 1.3.1

--- precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
+++ precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
@@ -41,4 +41,7 @@ def train(num_epochs, model, optimizer, train_loader, val_loader, fabric, schedu
                 train_acc.update(predicted_labels, targets)
         scheduler.step()
 
-     
+        ### MORE LOGGING
+        model.eval()
+        with torch.no_grad():
+            val_acc = torchmetrics.Accuracy(task="
