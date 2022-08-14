5160af70bb8e443d228332dee3d6e037ceed4f04
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-08-14 05:48:31 +0000

    chore: Upgrade

--- precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
+++ precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
@@ -47,4 +47,6 @@ def train(num_epochs, model, optimizer, train_loader, val_loader, fabric, schedu
             val_acc = torchmetrics.Accuracy(task="multiclass", num_classes=10).to(fabric.device)
 
             for (features, targets) in val_loader:
-                outputs = model(
+                outputs = model(features)
+                predicted_labels = torch.argmax(outputs, 1)
+                val_acc.update(predicted_labels
