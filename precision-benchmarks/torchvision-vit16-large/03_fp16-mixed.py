b4a0dcbf7c3628309e449990ab83aa4689708f8f
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-08-27 11:04:16 +0000

    adjust ui

--- precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
+++ precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
@@ -87,4 +87,9 @@ if __name__ == "__main__":
     ### 2 Initializing the Model
     #########################################
 
-    model = vit_l_16(weights=ViT_L_16_Weights.IMAGENET1K_V1
+    model = vit_l_16(weights=ViT_L_16_Weights.IMAGENET1K_V1)
+
+    # replace output layer
+    model.heads.head = torch.nn.Linear(in_features=1024, out_features=10)
+
+    o
