5372b07b5f21f25deb3461d4d66fe17f3a3f28a7
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-09-06 05:40:13 +0000

    Fix UI

--- precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
+++ precision-benchmarks/torchvision-vit16-large/03_fp16-mixed.py
@@ -124,4 +124,7 @@ if __name__ == "__main__":
     end = time.time()
     elapsed = end-start
     fabric.print(f"Time elapsed {elapsed/60:.2f} min")
-    fabric.prin
+    fabric.print(f"Memory used: {torch.cuda.max_memory_reserved() / 1e9:.02f} GB")
+
+    #########################################
+    
