baa9496f65e8261f90a00293b1574c69c1b6bcc5
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-09-27 19:40:58 +0000

    New translations django.po (Japanese)

--- precision-benchmarks/torchvision-vit16-large/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-large/local_utilities.py
@@ -2,4 +2,9 @@ import torch
 from torch.utils.data import sampler
 from torchvision import datasets
 from torch.utils.data import DataLoader
-from torch.utils.data import SubsetRando
+from torch.utils.data import SubsetRandomSampler
+from torchvision import transforms
+
+
+def get_dataloaders_cifar10(batch_size, num_workers=0,
+                     
