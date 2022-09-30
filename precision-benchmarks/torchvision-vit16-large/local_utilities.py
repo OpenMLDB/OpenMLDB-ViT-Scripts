172e9aef6af9676e04de8b4325f74218e389ce96
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-09-30 06:40:11 +0000

    new test file

--- precision-benchmarks/torchvision-vit16-large/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-large/local_utilities.py
@@ -13,4 +13,7 @@ def get_dataloaders_cifar10(batch_size, num_workers=0,
                             download=True):
 
     if train_transforms is None:
-        train_transf
+        train_transforms = transforms.ToTensor()
+
+    if test_transforms is None:
+        test_transforms = transfo
