d117224be6423f700712e2438140182e28cd98a6
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-10-06 22:17:38 +0000

    Modified   ManaPHP/helpers.php

--- precision-benchmarks/torchvision-vit16-large/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-large/local_utilities.py
@@ -44,4 +44,6 @@ def get_dataloaders_cifar10(batch_size, num_workers=0,
                                   num_workers=num_workers,
                                   sampler=valid_sampler)
 
-   
+        train_loader = DataLoader(dataset=train_dataset,
+                                  batch_size=batch_size,
+       
