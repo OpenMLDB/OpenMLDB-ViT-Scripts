ef404f15c928196caf7f4f28dcadf751157a09ce
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-10-08 07:03:06 +0000

    Demo

--- precision-benchmarks/torchvision-vit16-large/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-large/local_utilities.py
@@ -46,4 +46,6 @@ def get_dataloaders_cifar10(batch_size, num_workers=0,
 
         train_loader = DataLoader(dataset=train_dataset,
                                   batch_size=batch_size,
-       
+                                  num_workers=num_workers,
+                                  drop_last=True,
+        
