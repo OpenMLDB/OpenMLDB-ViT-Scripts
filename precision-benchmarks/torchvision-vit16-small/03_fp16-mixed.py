80c9c6ed83c83e3dc6d7d633ada482d24e716446
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-10-31 14:37:05 +0000

    Clone from zip

--- precision-benchmarks/torchvision-vit16-small/03_fp16-mixed.py
+++ precision-benchmarks/torchvision-vit16-small/03_fp16-mixed.py
@@ -33,4 +33,7 @@ def train(num_epochs, model, optimizer, train_loader, val_loader, fabric, schedu
 
             ### LOGGING
             if not batch_idx % 300:
-                fabric.print(f"Epoch: {epoch+1:04d}/{num_epochs:04d} | Batch {batch_idx:0
+                fabric.print(f"Epoch: {epoch+1:04d}/{num_epochs:04d} | Batch {batch_idx:04d}/{len(train_loader):04d} | Loss: {loss:.4f}")
+
+            model.eval()
+            with torch.no_grad():
