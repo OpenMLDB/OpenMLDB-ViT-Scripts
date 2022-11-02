b570ac07bb80f2eb71ef563f077dd5cb106951dc
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-11-02 05:42:28 +0000

    PR cleanup

--- precision-benchmarks/torchvision-vit16-small/03_fp16-mixed.py
+++ precision-benchmarks/torchvision-vit16-small/03_fp16-mixed.py
@@ -37,3 +37,5 @@ def train(num_epochs, model, optimizer, train_loader, val_loader, fabric, schedu
 
             model.eval()
             with torch.no_grad():
+                predicted_labels = torch.argmax(logits, 1)
+                train_acc.update(predicted_labels, targets
