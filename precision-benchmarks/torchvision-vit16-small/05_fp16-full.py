c1da6b88bfae1d014052a50f6e46ec7081e067e6
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-12-06 17:16:43 +0000

    Add resources.

--- precision-benchmarks/torchvision-vit16-small/05_fp16-full.py
+++ precision-benchmarks/torchvision-vit16-small/05_fp16-full.py
@@ -1,4 +1,8 @@
 import time
 
 import lightning as L
-from lightning 
+from lightning import Fabric
+import torch
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import ExponentialLR
+im
