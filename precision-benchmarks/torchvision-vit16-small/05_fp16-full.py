a1429eac9e46b7a0ce989bdbf5bf697167895ffe
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2022-12-23 15:10:12 +0000

    Fix bootstrap

--- precision-benchmarks/torchvision-vit16-small/05_fp16-full.py
+++ precision-benchmarks/torchvision-vit16-small/05_fp16-full.py
@@ -68,4 +68,6 @@ if __name__ == "__main__":
     ##########################
     train_transforms = transforms.Compose([transforms.Resize((224, 224)),
                                            #transforms.RandomCrop((224, 224)),
-            
+                                           transforms.ToTensor()])
+
+    test_transforms = transforms.Compose
