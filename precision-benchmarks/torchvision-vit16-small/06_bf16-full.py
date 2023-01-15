9b90ded770593b9e0c3348416259467b9c2e8abf
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2023-01-15 14:48:26 +0000

    Update Performance.md

--- precision-benchmarks/torchvision-vit16-small/06_bf16-full.py
+++ precision-benchmarks/torchvision-vit16-small/06_bf16-full.py
@@ -16,4 +16,8 @@ from local_utilities import get_dataloaders_cifar10
 def train(num_epochs, model, optimizer, train_loader, val_loader, fabric, scheduler):
 
     for epoch in range(num_epochs):
-        train_acc = torchmetrics.Accuracy(task="multiclass", num_classes=10).to(fabri
+        train_acc = torchmetrics.Accuracy(task="multiclass", num_classes=10).to(fabric.device)
+
+        model.train()
+        for batch_idx, (features, targets) in enumerate(train_loader):
+            
