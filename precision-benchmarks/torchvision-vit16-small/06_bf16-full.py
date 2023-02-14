747b0b90a62706244177ec1598a77d463d2b86e9
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2023-02-14 12:39:44 +0000

    add clear button

--- precision-benchmarks/torchvision-vit16-small/06_bf16-full.py
+++ precision-benchmarks/torchvision-vit16-small/06_bf16-full.py
@@ -97,4 +97,9 @@ if __name__ == "__main__":
 
     #########################################
     ### 3 Launch Fabric
-    ########################################
+    #########################################
+
+    fabric = Fabric(accelerator="cuda", devices=1, precision="bf16-true")
+    fabric.launch()
+
+    train_loader
