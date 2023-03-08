4d06b04e1002421a3b116ce26e77d22fec9ab0c6
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2023-03-08 12:51:44 +0000

    Promote CBS tags update for stein-testing

--- precision-benchmarks/torchvision-vit16-small/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-small/local_utilities.py
@@ -1,4 +1,6 @@
 import torch
 from torch.utils.data import sampler
 from torchvision import datasets
-from torch.ut
+from torch.utils.data import DataLoader
+from torch.utils.data import SubsetRandomSampler
+from torchvision im
