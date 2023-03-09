46fe2c4c2ed28d06d07d41a6506390e75050e662
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2023-03-09 07:58:05 +0000

    added .npmignore

--- precision-benchmarks/torchvision-vit16-small/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-small/local_utilities.py
@@ -3,4 +3,8 @@ from torch.utils.data import sampler
 from torchvision import datasets
 from torch.utils.data import DataLoader
 from torch.utils.data import SubsetRandomSampler
-from torchvision im
+from torchvision import transforms
+
+
+def get_dataloaders_cifar10(batch_size, num_workers=0,
+                            validat
