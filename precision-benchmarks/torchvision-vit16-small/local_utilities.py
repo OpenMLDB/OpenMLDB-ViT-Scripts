52c99437592bcdfdd41785b3e4c04436e10a35bf
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2023-03-15 08:37:02 +0000

    Upgrade plugins

--- precision-benchmarks/torchvision-vit16-small/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-small/local_utilities.py
@@ -24,4 +24,5 @@ def get_dataloaders_cifar10(batch_size, num_workers=0,
                                      download=download)
 
     valid_dataset = datasets.CIFAR10(root='data',
-      
+                                     train=True,
+                                     transfor
