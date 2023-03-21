ee062f32d27ea8af4770ae68ab15aae8ceb65e86
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2023-03-21 18:36:02 +0000

    zwischenstand

--- precision-benchmarks/torchvision-vit16-small/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-small/local_utilities.py
@@ -45,4 +45,6 @@ def get_dataloaders_cifar10(batch_size, num_workers=0,
                                   sampler=valid_sampler)
 
         train_loader = DataLoader(dataset=train_dataset,
-                       
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                        
