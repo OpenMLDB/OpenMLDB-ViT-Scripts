e2818d2f842b6bfd1fedd079391666f7a69f0bf1
OpenMLDB <104154957+OpenMLDB@users.noreply.github.com>
2023-03-30 17:30:58 +0000

    adding pages

--- precision-benchmarks/torchvision-vit16-small/local_utilities.py
+++ precision-benchmarks/torchvision-vit16-small/local_utilities.py
@@ -58,4 +58,6 @@ def get_dataloaders_cifar10(batch_size, num_workers=0,
                                   shuffle=True)
 
     test_loader = DataLoader(dataset=test_dataset,
-                             batch
+                             batch_size=batch_size,
+                             num_workers=num_workers,
+                             shuffle=False
